9d9dd9cf9ff514f6b5f251d14ba77a1a010311a7
Mohammed Arham <mohammed.arham273@gmail.com>
2023-12-27 21:23:43 +0530

    first commit

---
+++ src/CounterApp.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class CounterApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: props.count
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default CounterApp;
